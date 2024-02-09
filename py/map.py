6686dead609897312f13f59337e9cca09f830218
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-09 20:09:57 +0000

    added and changed some python dependecies

--- py/map.py
+++ py/map.py
@@ -2,8 +2,19 @@ global Zone
 Zone = 1
 global Room
 Room = 1
+global mapPos
+mapPos = [3,3]
 
 from rython import *
 global Firstmap
 Firstmap = Matrix.new(5)
 Firstmap.modifyPos([3,3],1)
+
+import keyboard  # using module keyboard
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+            print('You Pressed A Key!')
+            break  # finishing the loop
+    except:
+        break  # if user pressed a key other than the given key the loop will break
