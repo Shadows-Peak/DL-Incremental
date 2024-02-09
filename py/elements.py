090510437e8d246841519b86b82941c451966255
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-09 22:22:27 +0000

    changing entire base structure to not rely on MrRedShark77's Really Grass Cutting Incremental code as much

---
+++ py/elements.py
@@ -0,0 +1,7 @@
+from pyscript import when 
+
+#@when('click', "#btn")
+#def set_text_area():
+    #content = Element("inp").element.value
+    #content = content * 2 # do some text processing here
+    #Element("area").element.value = content
--- py/map.py
+++ py/map.py
@@ -10,11 +10,21 @@ global Firstmap
 Firstmap = Matrix.new(5)
 Firstmap.modifyPos([3,3],1)
 
-import keyboard  # using module keyboard
-while True:  # making a loop
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-            print('You Pressed A Key!')
-            break  # finishing the loop
-    except:
-        break  # if user pressed a key other than the given key the loop will break
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
