537c8e154f4be474be039802f1565c919bc82d09
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-13 20:19:48 +0000

    testing the changing of html elements

--- py/map.py
+++ py/map.py
@@ -1,4 +1,6 @@
 import pynput
+from pyscript import Element
+chart_element = Element("charts")
 
 global Zone
 Zone = 1
@@ -13,10 +15,12 @@ global Firstmap
 #Firstmap.modifyPos([3,3],1)
 
 def on_press(key):
+    chart_element.element.innerHTML = "This is <i>another</i> test."
     print('{0} pressed'.format(
         key))
 
 def on_release(key):
+    chart_element.element.innerHTML = "This is <i>yet another</i> test."
     print('{0} release'.format(
         key))
     if key == pynput.keyboard.Key.esc:
