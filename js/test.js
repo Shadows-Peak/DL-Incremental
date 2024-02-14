f05595e234f5cb6752464512bbaf24bd5a5c5c37
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-14 21:37:13 +0000

    change the text in accordance to W,A,S,D inputs, as well as arrow keys.

--- js/test.js
+++ js/test.js
@@ -1,7 +1,10 @@
 function changeText(text){
-    document.getElementById("charts").innerHTML = "New text!";
+    document.getElementById("charts").innerHTML = text;
 }
 
 window.addEventListener('keydown', e=>{
     if (e.key == "w" || e.key == "W" || e.key == "ArrowUp") changeText("Thanks for pressing: "+e.key)
+    if (e.key == "a" || e.key == "A" || e.key == "ArrowLeft") changeText("Thanks for pressing: "+e.key)
+    if (e.key == "s" || e.key == "S" || e.key == "ArrowDown") changeText("Thanks for pressing: "+e.key)
+    if (e.key == "d" || e.key == "D" || e.key == "ArrowRight") changeText("Thanks for pressing: "+e.key)
 })
