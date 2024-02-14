9d1bffe0b04957d0800703d50e4572fa93dca920
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-14 22:13:25 +0000

    removed the doctype html and added more semicolons.

--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
 function changeText(text){
     document.getElementById("charts").innerHTML = text;
-}
+};
 
 window.addEventListener('keydown', e=>{
     if (e.key == "w" || e.key == "W" || e.key == "ArrowUp") changeText("Thanks for pressing: "+e.key)
     if (e.key == "a" || e.key == "A" || e.key == "ArrowLeft") changeText("Thanks for pressing: "+e.key)
     if (e.key == "s" || e.key == "S" || e.key == "ArrowDown") changeText("Thanks for pressing: "+e.key)
     if (e.key == "d" || e.key == "D" || e.key == "ArrowRight") changeText("Thanks for pressing: "+e.key)
-})
+});
 
 document.getElementById('button1').onclick = function() {
     clicks++;
