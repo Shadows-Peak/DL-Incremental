05ab90e566f77e1b06c949b03d2f21f957a2669c
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-26 18:02:44 +0000

    more things attempt to draw to the counter, and there is a reset button

--- js/saveData.js
+++ js/saveData.js
@@ -2,11 +2,25 @@ function saveData() {
     localStorage.setItem('mainClicks', clicks);
 }
 
+function resetData() {
+    clicks = 0;
+    localStorage.setItem('mainClicks', 0);
+    updateVisuals();
+}
+
 document.getElementById('saveButton').onclick = function() {
     localStorage.setItem('mainClicks', clicks);
     document.getElementById('saveButton').innerHTML = "Saved!";
     setTimeout(function() {
       document.getElementById('saveButton').innerHTML = "Save Game";
+    }, (3 * 1000));
+};
+
+document.getElementById('resetButton').onclick = function() {
+    resetData();
+    document.getElementById('resetButton').innerHTML = "Game has been Reset!";
+    setTimeout(function() {
+      document.getElementById('resetButton').innerHTML = "Reset Game";
     }, (3 * 1000));
 };
 
