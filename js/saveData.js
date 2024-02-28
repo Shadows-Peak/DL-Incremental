1f26e98153971ede49fdc51f738ecc357e73d2fa
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-02-28 18:57:01 +0000

    added country clubs to save data and optimized saving code

--- js/saveData.js
+++ js/saveData.js
@@ -1,30 +1,49 @@
+data = {
+    "mainClicks": 0,
+    "CountryClubs": 0
+}
+
 function updateVisuals() {
-    document.getElementById('counter').innerHTML = "You have: <b>"+clicks+"</b> clicks";
+    document.getElementById('counter').innerHTML = "You have: <b>" + clicks + "</b> clicks";
+    document.getElementById('CountryClubButton').innerHTML = "Buy Country Club (" + CountryClubs + "): Cost: <b>" + CountryClubCost + "</b>";
 }
 
 function saveData() {
-    localStorage.setItem('mainClicks', clicks);
+    var basicIter = 0;
+    for (const key of Object.keys(data)) {
+        data[key] = [clicks,CountryClubs][basicIter];
+        basicIter++;
+    }
+    for (const [key, value] of Object.entries(data)) {
+        localStorage.setItem(key, value);
+    }
 }
 
 function resetData() {
-    clicks = 0;
-    localStorage.setItem('mainClicks', 0);
+    data = {
+        "mainClicks": 0,
+        "CountryClubs": 0
+    }
+    for (const [key, value] of Object.entries(data)) {
+        eval(key + " = " + value);
+        localStorage.setItem(key, value);
+    }
     updateVisuals();
 }
 
-document.getElementById('saveButton').onclick = function() {
+document.getElementById('saveButton').onclick = function () {
     localStorage.setItem('mainClicks', clicks);
     document.getElementById('saveButton').innerHTML = "Saved!";
-    setTimeout(function() {
-      document.getElementById('saveButton').innerHTML = "Save Game";
+    setTimeout(function () {
+        document.getElementById('saveButton').innerHTML = "Save Game";
     }, (3 * 1000));
 };
 
-document.getElementById('resetButton').onclick = function() {
+document.getElementById('resetButton').onclick = function () {
     resetData();
     document.getElementById('resetButton').innerHTML = "Game has been Reset!";
-    setTimeout(function() {
-      document.getElementById('resetButton').innerHTML = "Reset Game";
+    setTimeout(function () {
+        document.getElementById('resetButton').innerHTML = "Reset Game";
     }, (3 * 1000));
 };
 
@@ -32,9 +51,9 @@ function periodicSave() {
     saveData();
 }
 
-window.onbeforeunload = function(){
+window.onbeforeunload = function () {
     saveData();
     alert("Your game has been saved.")
 }
 
-setInterval(periodicSave, 120*1000);
+setInterval(periodicSave, 120 * 1000);
