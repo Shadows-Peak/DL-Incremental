4f6585c9449dd83d660c8a5346eacf59c374bf9e
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-03 08:30:58 +0000

    ege

--- js/initialize.js
+++ js/initialize.js
@@ -145,7 +145,7 @@ function offlineProgress() {
         clicks += Math.floor(0.2*(Math.floor(timeDifferenceSeconds) * (gameTick) * (AutomaticRizzers) * (1 + RiceWashers)));
         lastOfflineTime = 0;
 
-        alert("You gained "+Math.floor(0.2*(Math.floor(timeDifferenceSeconds) * (AutomaticRizzers) * (1 + RiceWashers)))+" clicks while you were gone! "));
+        alert("You gained "+Math.floor(0.2*(Math.floor(timeDifferenceSeconds) * (AutomaticRizzers) * (1 + RiceWashers)))+" clicks while you were gone! ");
         updateVisuals();
     }
 }
