327dba1753b87b26b953808dbd2beb8c1025796e
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-03 09:11:40 +0000

    e

--- js/settings.js
+++ js/settings.js
@@ -10,5 +10,6 @@ document.getElementById('ThemeChangeButton').onclick = function() {
     } else if (chosenBackground == 2) {
         chosenBackground = 1;
     }
+    document.getElementById('ThemeChangeButton').innerHTML = "Current Theme: "+["Light","Dark"][chosenBackground-1];
     updateBackgrounds();
 };
