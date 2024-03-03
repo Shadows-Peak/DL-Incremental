a307be09bdd6eac512cc7cc89eef87763a1fe51b
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-03 07:21:38 +0000

    rounds down what you produce

--- js/initialize.js
+++ js/initialize.js
@@ -142,10 +142,10 @@ function offlineProgress() {
         } else {
             var timeDifferenceSeconds = OfflineProdHrs*360;
         }
-        clicks += (timeDifferenceSeconds) * (gameTick) * (AutomaticRizzers) * (1 + RiceWashers);
+        clicks += Math.floor((timeDifferenceSeconds) * (gameTick) * (AutomaticRizzers) * (1 + RiceWashers));
         lastOfflineTime = 0;
 
-        alert("You gained "+((timeDifferenceSeconds) * (AutomaticRizzers) * (1 + RiceWashers))+" clicks while you were gone!");
+        alert("You gained "+Math.floor((timeDifferenceSeconds) * (AutomaticRizzers) * (1 + RiceWashers))+" clicks while you were gone!");
         updateVisuals();
     }
 }
