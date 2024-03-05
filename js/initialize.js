830e67615df1828d32c063c1c23e3b8d27990ea4
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-05 20:05:10 +0000

    stuff

--- js/initialize.js
+++ js/initialize.js
@@ -301,6 +301,6 @@ if (typeof console  != "undefined")
 
 console.log = function(message) {
     console.olog(message);
-    $('#debugDiv').append('<p>' + message + '</p>');
+    document.getElementById('debugDiv').innerHTML += ('<p>' + message + '</p>');
 };
 console.error = console.debug = console.info =  console.log
