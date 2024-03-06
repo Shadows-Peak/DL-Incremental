77cd6a22fb5d510518fda40f25b2e3b65bcd0711
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-06 20:19:48 +0000

    getting rid of the div for console messages

--- js/initialize.js
+++ js/initialize.js
@@ -339,16 +339,3 @@ function offlineProgress() {
         updateVisuals();
     }
 }
-
-
-if (typeof console  != "undefined") 
-    if (typeof console.log != 'undefined')
-        console.olog = console.log;
-    else
-        console.olog = function() {};
-
-console.log = function(message) {
-    console.olog(message);
-    document.getElementById('debugDiv').innerHTML += ('<p>' + message + '</p>');
-};
-console.error = console.debug = console.info =  console.log
