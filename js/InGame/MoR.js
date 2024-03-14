1b28c741f5cc51e6265fd27f9d06141df87b0721
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-14 20:39:26 +0000

    fsagwsgswgw

--- js/InGame/MoR.js
+++ js/InGame/MoR.js
@@ -1,6 +1,6 @@
 // Mine of Rizz
-function clickedMoRCell([x,y]) {
-  if (MoRCellHighlight == [x,y]) {
+function clickedMoRCell(x) {
+  if (MoRCellHighlight == x) {
     if (clicks >= 10000000 && RizzPoints >= 50 && RizziteNRizzium[0] < 10) {
       clicks -= 10000000;
       RizzPoints -= 50;
