afb7f7d4d95a5dfe771f3b4af41bc7623af58d1f
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-03-18 03:00:00 +0000

    added google drive api

--- js/initialize.js
+++ js/initialize.js
@@ -34,20 +34,69 @@ var MoRCellHighlight = [1,1];
 var RizziteNRizzium = [0,0,0]; // Rizzite Progress, Rizzite, Rizzium
 
 data2 = {
-    "clicks" : clicks,
-    "RizzalurgyUnlocked" : RizzalurgyUnlocked,
-    "RizzmaxExtraChance" : RizzmaxExtraChance
+    "clicks" : "clicks",
+    "RizzalurgyUnlocked" : "RizzalurgyUnlocked",
+    "RizzmaxExtraChance" : "RizzmaxExtraChance"
 };
 
 let translatedData = {};
 
 for (const [key, value] of Object.entries(data2)) {
     if (typeof window[key] !== "undefined") {
-        translatedData[key] = value;
+        translatedData[key] = window[value];
     }
 }
 
 console.log(translatedData);
+
+
+
+const { google } = require('googleapis');
+const path = require('path');
+const fs = require('fs');
+
+const CLIENT_ID = '271762087593-8v8c5fmtf48719m7csr592iphlstmibh.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
+
+const REFRESH_TOKEN = '1//04lS7a7V-8v-2CgYIARAAGAQSNwF-L9IrISi8lZBRaDm7h_soDG7f76_aZa5en7ffaVpnE3-vvnib75VxaTUe6PVi9U6jpr-FPIA';
+
+const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URL
+);
+
+oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+
+const drive = google.drive({
+    version: 'v3',
+    auth: oauth2Client
+});
+
+const filePath = path.join(__dirname, '../images/otherDilyanLopez.jpg');
+
+async function uploadFile() {
+    try {
+        const response = await drive.files.create({
+            requestBody: {
+                name: 'DLImage.jpg',
+                mimeType: 'image/jpg'
+            },
+            media: {
+                mimeType: 'image/jpg',
+                body: fs.createReadStream(filePath)
+            }
+         })
+    
+         console.log(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+uploadFile();
+
 
 
 function grabCost(Item) {
